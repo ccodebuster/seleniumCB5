d9a38c1412021145bae0f36ccb7b5be0cedc07f3
Perishblue <visnudasdarshan@gmail.com>
2024-05-19 16:05:23 +0100

    practice2

--- src/gitdemo/ShubhamGitDemo.java
+++ src/gitdemo/ShubhamGitDemo.java
@@ -2,6 +2,9 @@ package gitdemo;
 
 public class ShubhamGitDemo {
     public static void main(String[] args) {
+
         System.out.println("I like Java");
+
+        System.out.println("Kai pan chalse");
     }
 }
